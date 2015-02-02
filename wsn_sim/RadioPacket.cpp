1fabdb69b24a9f911959055aebfd2ad8b035bda8
Trond Einar Snekvik <trondesn@stud.ntnu.no>
2015-02-02 22:35:25 +0100

    RadioPacket: added collision logic and made << operator

--- wsn_sim/RadioPacket.cpp
+++ wsn_sim/RadioPacket.cpp
@@ -1,11 +1,12 @@
 #include "RadioPacket.h"
+#include "Radio.h"
 
 
-RadioPacket::RadioPacket(Radio* sender, uint8_t* data, uint32_t length) : mSender(sender), mLength(length), p_mEnvironment(sender.getEnvironment())
+RadioPacket::RadioPacket(const Radio* sender, uint8_t* data, uint32_t length) : mSender(sender), mLength(length), p_mEnvironment(sender->getEnvironment())
 {
   mData = new uint8_t[length];
   memcpy_s(mData, length, data, length);
-  mStartTime = p_mEnvironment->getTimestamp();
+  mStartTime = p_mEnvironment->getTimestamp();  
   mSignalStrength = sender->getSignalStrength();
 }
 
@@ -20,6 +21,33 @@ RadioPacket::~RadioPacket()
 
 bool RadioPacket::collidesWith(RadioPacket* pOther)
 {
-  if (this->mStartTime > pOther->mStartTime && pOther->mEndTime > this->mStartTime) //TODO
+  return (
+      (this != pOther) && 
+    (
+      (this->mStartTime >= pOther->mStartTime && this->mStartTime >= pOther->mEndTime) ||
+      (this->mStartTime >= pOther->mStartTime && this->mStartTime <= pOther->mEndTime) ||
+      (this->mStartTime <= pOther->mStartTime && this->mStartTime >= pOther->mEndTime) ||
+      (this->mStartTime <= pOther->mStartTime && this->mStartTime <= pOther->mEndTime) 
+    ) &&
+    !(
+      (this->mStartTime > pOther->mEndTime) ||
+      (pOther->mStartTime > this->mEndTime)
+    )
+    );
+}
 
+
+std::ostream& operator<<(std::ostream& ostr, RadioPacket& packet)
+{
+  ostr.setf(std::ios::hex, std::ios::basefield);
+  ostr.setf(std::ios::showbase);
+
+  for (uint32_t i = 0; i < packet.mLength; ++i)
+  {
+    ostr << packet.mData[i] << " ";
+  }
+
+  ostr << std::resetiosflags(std::ios::basefield) << std::resetiosflags(std::ios::showbase);
+
+  return ostr;
 }
--- wsn_sim/RadioPacket.h
+++ wsn_sim/RadioPacket.h
@@ -1,15 +1,18 @@
 #pragma once
 #include "Device.h"
-#include "Radio.h"
 #include "WSN.h"
 #include <stdint.h>
+#include <iostream>
+#include <iomanip>
 
 #define RADIO_SIGNAL_DECAY (1.0)
+
+class Radio;
 
 class RadioPacket
 {
 public:
-  RadioPacket(Radio* sender, uint8_t* data, uint32_t length);
+  RadioPacket(const Radio* sender, uint8_t* data, uint32_t length);
   RadioPacket(const RadioPacket& packet);
   ~RadioPacket();
 
@@ -17,14 +20,20 @@ public:
 
   bool collidesWith(RadioPacket* pOther);
 
+  const uint8_t* getContents(void) const { return mData; }
+  const uint32_t getLength(void) const { return mLength; }
+
   const Radio* getSender(void) { return mSender; }
 
-private:
+  friend std::ostream& operator<<(std::ostream& ostream, RadioPacket& packet);
+  
+
   uint32_t mStartTime;
   uint32_t mEndTime;
+private:
   SimEnv* p_mEnvironment;
   uint8_t mSignalStrength;
-  Radio* mSender;
+  const Radio* mSender;
   uint8_t* mData;
   uint32_t mLength;
 };
