66108afe313157cf59f47c816fbc197986d4b507
Trond Einar Snekvik <trondesn@stud.ntnu.no>
2015-02-04 11:30:04 +0100

    Added radio logic

--- wsn_sim/Radio.cpp
+++ wsn_sim/Radio.cpp
@@ -9,11 +9,13 @@ Radio::Radio(Device* device, uint8_t sigStrength, uint32_t turnaroundTime_us, ui
   mDevice(device), 
   mSigStrength(sigStrength), 
   mTurnaroundTime_us(turnaroundTime_us), 
-  mTifs_us(tifs_us),
   mBitrate(bitrate),
-  mState(RADIO_STATE_IDLE)
+  mState(RADIO_STATE_IDLE),
+  mShort(SHORT_DISABLED),
+  mNextStateTime(0)
 {
-  
+  // can't be faster than hardware allows
+  mTifs_us = (tifs_us > turnaroundTime_us) ? tifs_us : turnaroundTime_us;
 }
 
 
@@ -21,14 +23,133 @@ Radio::~Radio()
 {
 }
 
-void Radio::receivePacket(RadioPacket* pPacket)
-{
 
+void Radio::transmit(void)
+{
+  if (mState == RADIO_STATE_IDLE)
+  {
+    mState = RADIO_STATE_RAMPUP_TX;
+    mNextStateTime = getEnvironment()->getTimestamp() + mTurnaroundTime_us;
+  }
+  else
+  {
+    LOG_ERROR << "Radio TX ordered outside IDLE state (state: " << mState << ")";
+  }
+}
+
+void Radio::receive(void)
+{
+  if (mState == RADIO_STATE_IDLE)
+  {
+    mState = RADIO_STATE_RAMPUP_RX;
+    mNextStateTime = getEnvironment()->getTimestamp() + mTurnaroundTime_us;
+  }
+  else
+  {
+    LOG_ERROR << "Radio RX ordered outside IDLE state (state: " << mState << ")";
+  }
+}
+
+void Radio::disable(void)
+{
+  if (mState == RADIO_STATE_RX)
+  {
+    mWSN->removeReceiver(this);
+  }
+  else if (mState == RADIO_STATE_TX)
+  {
+    mWSN->abortTransmit(mTxPacketHandle);
+  }
+  mState = RADIO_STATE_IDLE;
+}
+
+void Radio::setPacket(uint8_t* packet, uint8_t length)
+{
+  mCurrentPacket = RadioPacket(this, packet, length);
+}
+
+void Radio::shortToRx(void)
+{
+  mShort = SHORT_TO_RX;
+}
+
+void Radio::shortToTx(void)
+{
+  mShort = SHORT_TO_TX;
+}
+
+void Radio::shortDisable(void)
+{
+  mShort = SHORT_DISABLED;
+}
+
+void Radio::receivePacket(RadioPacket* pPacket, uint8_t rx_strength, bool corrupted)
+{
+  mDevice->radioCallbackRx(pPacket, rx_strength, corrupted);
 
   LOG_RX << pPacket;
 }
 
 void Radio::step(uint32_t timestamp)
 {
+  if (timestamp >= mNextStateTime)
+  {
+    switch (mState)
+    {
+    case RADIO_STATE_IDLE:
+      // wait for external tx or rx call
+      mNextStateTime = UINT32_MAX;
+      break;
 
+    case RADIO_STATE_RAMPUP_RX:
+      mWSN->addReceiver(this);
+      mState = RADIO_STATE_RX;
+      break;
+
+    case RADIO_STATE_RAMPUP_TX:
+      mTxPacketHandle = mWSN->startTransmit(mCurrentPacket);
+      mNextStateTime = timestamp + getTxTime(mCurrentPacket);
+      mState = RADIO_STATE_TX;
+      break;
+
+    case RADIO_STATE_RX:
+      // is this possible?
+      mWSN->removeReceiver(this);
+      shortToNextState();
+      mDevice->radioCallbackRx(NULL, 0, true);
+      break;
+
+    case RADIO_STATE_TX:
+      mWSN->endTransmit(mTxPacketHandle);
+      shortToNextState();
+      mDevice->radioCallbackTx(&mCurrentPacket);
+      break;
+
+    default:
+      LOG_ERROR << "Radio: illegal state";
+    }
+  }
+
+}
+
+void Radio::shortToNextState(void)
+{
+  if (mShort == SHORT_DISABLED)
+  {
+    mState = RADIO_STATE_IDLE;
+    mNextStateTime = UINT32_MAX;
+  }
+  else
+  {
+    mNextStateTime = getEnvironment()->getTimestamp() + mTifs_us;
+
+    if (mShort == SHORT_TO_RX)
+    {
+      mState = RADIO_STATE_RAMPUP_RX;
+    }
+    else
+    {
+      mState = RADIO_STATE_RAMPUP_TX;
+    }
+  }
 }
--- wsn_sim/Radio.h
+++ wsn_sim/Radio.h
@@ -6,11 +6,18 @@
 
 #define RADIO_LOG_ENABLED (true)
 
+#define RADIO_DEFAULT_SIGSTRENGTH (100)
+#define RADIO_DEFAULT_TURNAROUND  (138)
+#define RADIO_DEFAULT_TIFS        (150)
+#define RADIO_DEFAULT_BITRATE     (1000)
+
 class Device;
 class RadioPacket;
 
+
 class Radio : public Runnable
 {
+  friend WSN;
 public:
   typedef enum
   {
@@ -24,28 +31,49 @@ public:
   Radio(Device* device, uint8_t sigStrength, uint32_t turnaroundTime_us, uint32_t tifs_us, uint32_t bitrate);
   ~Radio();
 
-  void transmit(uint8_t* packet, uint8_t length);
-  void receive(uint8_t* packet, uint8_t maxLength);
+  void transmit(void);
+  void receive(void);
   void disable(void);
+  void setPacket(uint8_t* packet, uint8_t length);
+
+
+  void shortToRx(void);
+  void shortToTx(void);
+  void shortDisable(void);
+
+  void setWSN(WSN* wsn) { this->mWSN = wsn; }
 
   uint8_t getSignalStrength(void) const { return mSigStrength; }
+  uint32_t getTxTime(RadioPacket& packetToTx) { return mBitrate * packetToTx.getLength() * 8; }
 
   double getX(void) { return mDevice->pos.x; }
   double getY(void) { return mDevice->pos.y; }
 
-  void receivePacket(RadioPacket* pPacket);
-
-  Device* getDevice(void) const { return mDevice; }
+  Device* const getDevice(void) const { return mDevice; }
 
   virtual void step(uint32_t time);
 
 
 private:
+  typedef enum
+  {
+    SHORT_TO_RX,
+    SHORT_TO_TX,
+    SHORT_DISABLED
+  }short_t;
+  short_t mShort;
   state_t mState;
   Device* mDevice;
+  RadioPacket mCurrentPacket;
+  packetHandle_t mTxPacketHandle;
+  WSN* mWSN;
   uint32_t mTurnaroundTime_us;
   uint32_t mBitrate;
   uint8_t mTifs_us;
   uint8_t mSigStrength;
+  uint32_t mNextStateTime;
+
+  void receivePacket(RadioPacket* pPacket, uint8_t rx_strength, bool corrupted);
+  void shortToNextState(void);
 };
 
