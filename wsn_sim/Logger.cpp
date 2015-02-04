dd40239a028ca892a39ed9bc193242eb68386989
Trond Einar Snekvik <trondesn@stud.ntnu.no>
2015-02-04 15:07:38 +0100

    sort of fixed the newline functionality on logger

--- wsn_sim/Logger.cpp
+++ wsn_sim/Logger.cpp
@@ -1,2 +1,8 @@
 #include "Logger.h"
 
+
+std::ostream& operator<<(std::ostream& os, const Logger& log)
+{
+  os << std::endl << log.mPrefix << ": " << log.mStream.str();
+  return os;
+}
--- wsn_sim/Logger.h
+++ wsn_sim/Logger.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <sstream>
 
 #define LOG_ENABLE        (true)
 
@@ -13,19 +14,30 @@ class Logger
 public:
   Logger(std::string prefix, bool enabled) : mPrefix(prefix), mEnabled(enabled) {};
 
+  Logger& operator<<(const Logger& log)
+  {
+    mStream << log.mStream.str();
+    return *this;
+  }
+
   template <class T>
   Logger& operator<<(const T &data)
   {
     if (mEnabled && LOG_ENABLE)
-      std::cout << mPrefix << ": " << data << std::endl;
-    std::string(__FUNCTION__);
+      mStream << data;
     return *this;
   }
+
+
+  friend std::ostream& operator<<(std::ostream& os, const Logger& log);
 private:
+  std::stringstream mStream;
   std::string mPrefix;
   bool mEnabled;
 };
 
-#define LOG_ERROR   Logger("[ERROR][" + std::string(__FILE__) + ":L" + std::to_string(__LINE__) + "]", true)
-#define LOG_DEBUG   Logger("[DEBUG][" + std::string(__FILE__) + ":L" + std::to_string(__LINE__) + "]", true)
-#define LOG_VERBOSE Logger("[VERBOSE][" + std::string(__FILE__) + ":L" + std::to_string(__LINE__) + "]", true)
+#define LOG_ERROR   std::cout << Logger("[ERROR][" + std::string(__FILE__) + ":L" + std::to_string(__LINE__) + "]", true)
+#define LOG_DEBUG   std::cout << Logger("[DEBUG][" + std::string(__FILE__) + ":L" + std::to_string(__LINE__) + "]", true)
+#define LOG_VERBOSE std::cout << Logger("[VERBOSE][" + std::string(__FILE__) + ":L" + std::to_string(__LINE__) + "]", true)
+
+#define LOGGER(prefix) std::cout << Logger(prefix, true)
