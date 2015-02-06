77b30e7ddb65fb4f8300010b462a86080df9ff3d
Trond Einar Snekvik <trondesn@stud.ntnu.no>
2015-02-06 15:07:34 +0100

    Made barrier thread safe, ironically

--- wsn_sim/Multithreading.h
+++ wsn_sim/Multithreading.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <mutex>
 #include <condition_variable>
+#include <string>
 
 class Barrier
 {
@@ -8,20 +9,41 @@ private:
 	std::mutex _mutex;
 	std::condition_variable _cv;
 	std::size_t _count;
+  std::mutex _writeMutex;
 public:
 	explicit Barrier(uint32_t count) : _count{ count } { }
-  Barrier() : Barrier(0){}
-  void reset(uint32_t count) { _count = count; }
+  Barrier() : Barrier(1){}
+  void reset(uint32_t count) 
+  { 
+    _writeMutex.lock();
+    _count = count; 
+    _writeMutex.unlock();
+  }
 
 	void wait()
 	{
 		std::unique_lock<std::mutex> lock{ _mutex };
+    _writeMutex.lock();
 		if (--_count == 0) {
+      _writeMutex.unlock();
 			_cv.notify_all();
 		}
-		else {
-			_cv.wait(lock, [this] { return _count == 0; });
+    else {
+      _writeMutex.unlock();
+			_cv.wait(lock);
 		}
 	}
+
+  void post()
+  {
+    _writeMutex.lock();
+    if (--_count == 0)
+    {
+      _cv.notify_all();
+    }
+    _writeMutex.unlock();
+  }
+
+  std::string ToString(void) { return "Count = " + std::to_string(_count); }
 };
 
