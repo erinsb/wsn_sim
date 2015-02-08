6d0c6bcb4f5b5c8411df751520c1334e057e7b60
Trond Einar Snekvik <trondesn@stud.ntnu.no>
2015-02-08 14:00:10 +0100

    put scandevice in adv/scan main

--- BLE_adv_scan_test/main.cpp
+++ BLE_adv_scan_test/main.cpp
@@ -1,4 +1,5 @@
 #include "AdvDevice.h"
+#include "ScanDevice.h"
 #include "WSN.h"
 #include <iostream>
 #include <fstream>
@@ -12,13 +13,17 @@ int main(void)
   uint8_t advMsg[] {1, 2, 3, 4};
   uint8_t scanMsg[] {5, 6, 7, 8};
   AdvDevice advDev(advMsg, 4, scanMsg, 4);
+  ScanDevice scanDev;
   wsn.addDevice(advDev);
+  wsn.addDevice(scanDev);
 
   advDev.start();
+  scanDev.start(30000, 30000);
 
-  simEnv.run(200000);
+  simEnv.run(2000000);
   
-  auto usage = advDev.getPowerUsage();
+#if 1
+  auto usage = scanDev.getPowerUsage();
 
   std::cout << std::endl;
   std::ofstream ofs;
@@ -27,7 +32,7 @@ int main(void)
     ofs << mA << std::endl;
 
   std::cout << advDev.getPowerUsageAvg() << "mA" << std::endl;
-
+#endif
   system("pause");
   return 0;
 }
