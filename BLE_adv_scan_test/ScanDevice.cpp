d798de9adea4527cb83ff165716a00d3a574ad0d
Trond Einar Snekvik <trondesn@stud.ntnu.no>
2015-02-08 13:58:54 +0100

    Added scan device logic in ble adv/scan

--- BLE_adv_scan_test/ScanDevice.cpp
+++ BLE_adv_scan_test/ScanDevice.cpp
@@ -1,11 +1,95 @@
 #include "ScanDevice.h"
+#include "Timer.h"
+#include "Radio.h"
+#include "Logger.h"
 
-
-ScanDevice::ScanDevice()
+ScanDevice::ScanDevice() 
 {
+  mScanRsp = new uint8_t[]{ 1, 2, 3, 4, 5 };
 }
 
 
 ScanDevice::~ScanDevice()
 {
+}
+
+void ScanDevice::start(uint32_t scanInterval, uint32_t scanTime) 
+{
+  mScanInterval = scanInterval;
+  mScanTime = scanTime;
+  if (mScanInterval < mScanTime)
+    mScanInterval = mScanTime;
+
+  
+  startScanning();
+}
+
+void ScanDevice::step(uint32_t timestamp)
+{
+  
+}
+
+void ScanDevice::startScanning(void)
+{
+  if (mScanInterval != mScanTime)
+    mTimer->orderRelative(mScanTime, std::bind(&ScanDevice::stopScanning, this));
+  mRadio->shortToTx();
+  mRadio->receive();
+  mWaitingForAdv = true;
+
+  mRadio->setTifs(150);
+  mRadio->setPacket(mScanRsp, 5);
+  _LOG("Start scanning");
+}
+
+void ScanDevice::stopScanning(void)
+{
+  mTimer->orderRelative(mScanInterval - mScanTime, std::bind(&ScanDevice::startScanning, this));
+  mRadio->shortDisable();
+  mRadio->disable();
+}
+
+void ScanDevice::abortRspRx()
+{
+  if (!mWaitingForAdv)
+  {
+    //just report scan rsp as failed, continue scanning
+    mWaitingForAdv = true;
+    _LOG("No scan rsp");
+    mRadio->shortToTx();
+    mRadio->setTifs(150);
+  }
+}
+
+void ScanDevice::radioCallbackTx(RadioPacket* packet)
+{
+  //_LOG("Done TX");
+  mTimer->orderRelative(600, std::bind(&ScanDevice::abortRspRx, this));
+  mRadio->shortToRx();
+  mRadio->setTifs(148);
+}
+
+void ScanDevice::radioCallbackRx(RadioPacket* packet, uint8_t rx_strength, bool corrupted)
+{
+  if (packet == NULL)
+  {
+    _LOG("NULL PACKET");
+    return;
+  }
+  if (mWaitingForAdv)
+  {
+    //do scan req
+    _LOG("Got adv");
+    mRadio->shortToRx();
+    mRadio->setTifs(148);
+    mWaitingForAdv = false;
+  }
+  else
+  {
+    //got scan rsp, do general scan
+    _LOG("Got scan rsp");
+    mRadio->shortToTx();
+    mRadio->setTifs(150);
+    mWaitingForAdv = true;
+  }
 }
--- BLE_adv_scan_test/ScanDevice.h
+++ BLE_adv_scan_test/ScanDevice.h
@@ -6,5 +6,21 @@ class ScanDevice : public Device
 public:
   ScanDevice();
   ~ScanDevice();
+
+  void start(uint32_t scanInterval, uint32_t scanTime);
+  
+  virtual void step(uint32_t timestamp);
+
+private:
+  uint32_t mScanInterval, mScanTime;
+  uint8_t* mScanRsp;
+  bool mWaitingForAdv;
+
+  void startScanning();
+  void stopScanning();
+  void abortRspRx();
+
+  virtual void radioCallbackTx(RadioPacket* packet);
+  virtual void radioCallbackRx(RadioPacket* packet, uint8_t rx_strength, bool corrupted);
 };
 
