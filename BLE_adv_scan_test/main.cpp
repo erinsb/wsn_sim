e227c3440176289dcbc670bce0ec671bffe2e39b
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2015-02-26 14:01:10 +0100

    BLE adv scan now includes 100 randomly placed Advertisers with custom intervals

--- BLE_adv_scan_test/main.cpp
+++ BLE_adv_scan_test/main.cpp
@@ -3,6 +3,10 @@
 #include "WSN.h"
 #include <iostream>
 #include <fstream>
+#include <vector>
+
+#define ADVDEV_COUNT  (100)
+#define AREA          (100)
 
 int main(void)
 {
@@ -12,24 +16,34 @@ int main(void)
 
   uint8_t advMsg[] {1, 2, 3, 4};
   uint8_t scanMsg[] {5, 6, 7, 8};
-  AdvDevice advDev(advMsg, 4, scanMsg, 4);
   ScanDevice scanDev;
-  wsn.addDevice(advDev);
   wsn.addDevice(scanDev);
 
-  advDev.start();
+  for (uint32_t i = 0; i < ADVDEV_COUNT; ++i)
+  {
+    AdvDevice* pDev = new AdvDevice(advMsg, 4, scanMsg, 4, rand() % 100000 + 100000);
+
+    pDev->pos.x = rand() % AREA;
+    pDev->pos.y = rand() % AREA;
+    wsn.addDevice(*pDev);
+  
+    pDev->start();
+  }
+  
+
   scanDev.start(30000, 30000);
 
-  simEnv.run(2000000);
+  simEnv.run(2 * SECONDS);
+
   
 #if 1
-  auto usage = scanDev.getPowerUsage();
+  auto usage = scanDev.getPowerUsageEvents();
 
   std::cout << std::endl;
   std::ofstream ofs;
   ofs.open("log.txt");
-  for (double mA : usage)
-    ofs << mA << std::endl;
+  for (Device::powerEvent_t& ev : usage)
+    ofs << ev.power_mA << "\t" << ev.timestamp << std::endl;
 
   std::cout << advDev.getPowerUsageAvg() << "mA" << std::endl;
 #endif
