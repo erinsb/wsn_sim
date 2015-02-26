e87bdd3d8cac92b0ccbe6d5fa5323290faec1315
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2015-02-26 13:58:59 +0100

    Changed SimEnv to event driven, ~500x speedup

--- wsn_sim/SimEnv.h
+++ wsn_sim/SimEnv.h
@@ -6,15 +6,25 @@
 #include <thread>
 #include <stdint.h>
 
+#define USE_THREADS   (0)
 #define THREAD_COUNT  (4)
+#define MS            (1000)
+#define SECONDS       (1000 * MS)
+#define MINUTES       (60 * SECONDS)
+#define HOURS         (60 * MINUTES)
 
 class Runnable;
+
+#if USE_THREADS
 class SimThread;
+#endif
 
 class SimEnv
 {
   friend Runnable;
+#if USE_THREADS
   friend SimThread;
+#endif
 
 public:
   SimEnv();
@@ -26,17 +36,34 @@ public:
   void stop(void) { mRunning = false; }
 
   void run(uint32_t stopTime = UINT32_MAX, uint32_t deltaTime = 1);
+  void registerExecution(Runnable* executor, uint32_t timestamp);
 
 private:
+  typedef struct
+  {
+    Runnable* pRunnable;
+    uint32_t timestamp;
+  } execution_t;
+
   uint32_t mTime;
+#if USE_THREADS
   std::vector<Runnable*> mTempRunnables;
   SimThread* mThreads[THREAD_COUNT];
   Barrier mStartBarrier, mEndBarrier;
   std::mutex mStartMut;
   std::condition_variable mStartCV;
+#else
+  std::vector<Runnable*> mRunnables;
+#endif
   bool mRunning = true;
+  std::vector<execution_t> mExecutionList;
+  uint32_t mNextExecution = 0;
+  bool mExecutionListInvalidated = false;
+  void step(void);
 };
 
+
+#if USE_THREADS
 class SimThread
 {
   friend SimEnv;
@@ -51,3 +78,5 @@ private:
   std::vector<Runnable*> mRunnables;
   void run(void);
 };
+#endif
+
