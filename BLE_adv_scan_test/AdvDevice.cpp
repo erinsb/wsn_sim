912ffe1b38d7b61de19b47afe07bc1eda41ded60
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2015-02-27 08:59:49 +0100

    Actually changed to BLE packets in advdevice

--- BLE_adv_scan_test/AdvDevice.cpp
+++ BLE_adv_scan_test/AdvDevice.cpp
@@ -8,11 +8,9 @@
 
 
 
-AdvDevice::AdvDevice(uint8_t* advPacket, uint32_t advPacketLength, uint8_t* scanPacket, uint32_t scanPacketLength, uint32_t advInt):
+AdvDevice::AdvDevice(ble_adv_packet_t* advPacket, ble_adv_packet_t* scanPacket, uint32_t advInt) :
   mAdvPacket(advPacket),
-  mAdvPacketLength(advPacketLength),
   mScanPacket(scanPacket),
-  mScanPacketLength(scanPacketLength),
   mAdvInt(advInt)
 {
 }
@@ -52,7 +50,7 @@ void AdvDevice::radioCallbackRx(RadioPacket* packet, uint8_t rx_strength, bool c
 {
   if (packet != NULL)
   {
-    mRadio->setPacket(mScanPacket, mScanPacketLength);
+    mRadio->setPacket((uint8_t*)mScanPacket, mScanPacket->length + BLE_PACKET_OVERHEAD_LENGTH);
     mRadio->shortDisable();
   }
 }
@@ -62,7 +60,7 @@ void AdvDevice::startAdv(uint32_t timestamp, void* context)
   _LOG("ADV %d", timestamp);
   mTimer->orderRelative(mAdvInt, std::bind(&AdvDevice::startAdv, this, std::placeholders::_1, std::placeholders::_2));
 
-  mRadio->setPacket(mAdvPacket, mAdvPacketLength);
+  mRadio->setPacket((uint8_t*)mAdvPacket, mAdvPacket->length + BLE_PACKET_OVERHEAD_LENGTH);
   mRadio->shortToRx();
   mRadio->setTifs(148);
   mRadio->transmit();
