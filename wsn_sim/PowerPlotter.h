c3e4b8b47dbcf21bc531a81fba80e8dee8639016
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2015-02-27 09:00:48 +0100

    Added matplotPP lib to plot power

---
+++ wsn_sim/PowerPlotter.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "matplotpp.h"
+#include "Device.h"
+
+class PowerPlotter : public MatPlot
+{
+public:
+  PowerPlotter::PowerPlotter(void) : mGrid(0){}
+
+  void specialKeys(int key, int x, int y);
+
+  void toggleGrid(void);
+  void addDevice(Device* pDev) { mDevices.push_back(pDev); }
+  void displayGraph(uint32_t startTime, uint32_t endTime, Device* pDev = NULL);
+  void DISPLAY(void);
+  uint32_t mStartTime, mEndTime;
+private:
+  int mGrid;
+  std::vector<Device*> mDevices;
+
+  void plotDevice(Device* pDev);
+};
+
