bb7fa698cfde7c814097db080d9a1c288bfa5f9d
Trond Einar Snekvik <trondesn@stud.ntnu.no>
2015-03-01 21:49:29 +0100

    Implemented more of mesh device

--- MeshTesting/MeshDevice.cpp
+++ MeshTesting/MeshDevice.cpp
@@ -2,11 +2,70 @@
 #include "BlePacket.h"
 #include "Radio.h"
 #include "RadioPacket.h"
+#include <random>
 
 #define SEARCHLIGHT_SLOT (10000)
 
+
+MeshNeighbor::MeshNeighbor(ble_adv_addr_t* adv_addr) : 
+  mLastSyncTime(0), 
+  mBeaconCount(0), 
+  mFollowing(0),
+  mNbCount(0),
+  mPowerScore(0)
+{
+  memcpy(&mAdvAddr, adv_addr, 6);
+}
+
+void MeshNeighbor::receivedBeacon(uint32_t rxTime, mesh_packet_t* beacon)
+{
+  ++mBeaconCount;
+  mLastSyncTime = rxTime;
+
+  // extract any state information 
+  switch (beacon->payload.str.adv_type)
+  {
+  case MESH_ADV_TYPE_DEFAULT:
+    if (beacon->payload.str.payload.default.clusterAddr != mClusterHead)
+    {
+      memcpy(mClusterHead.arr, beacon->payload.str.payload.default.clusterAddr.arr, BLE_ADV_ADDR_LEN);
+    }
+    mNbCount = beacon->payload.str.payload.default.nbCount;
+    mPowerScore = beacon->payload.str.payload.default.powerScore;
+    break;
+  default:
+    // don't care
+  }
+}
+
+bool MeshNeighbor::isClusterHead(void)
+{
+  return (mAdvAddr == mClusterHead);
+}
+
+uint32_t MeshNeighbor::getNextBeaconTime(uint32_t timeNow)
+{
+  if (timeNow <= mLastSyncTime)
+    return mLastSyncTime + MESH_INTERVAL;
+  else
+    return (1 + (timeNow - mLastSyncTime) / MESH_INTERVAL) * MESH_INTERVAL + mLastSyncTime;
+}
+
 MeshDevice::MeshDevice(uint8_t* defaultData, uint32_t defaultLength, double x, double y) : Device(x, y), mSearching(false)
 {
+  double driftFactor = (MESH_MAX_CLOCK_DRIFT) * (2.0 * ((double)rand() / RAND_MAX) - 1.0) + 1.0; // 1.0 +- <250/1mill 
+  mTimer->setDriftFactor(driftFactor);
+
+  for (uint8_t i = 0; i < BLE_ADV_ADDR_LEN; ++i)
+  {
+    mMyAddr.arr[i] = rand() & 0xFF;
+  }
+
+  for (uint8_t i = 0; i < MESH_MAX_ROUTES; ++i)
+  {
+    mRoutes[i].timeStamp = 0;
+    mRoutes[i].distance = MESH_UNKNOWN_DIST;
+  }
 }
 
 
@@ -32,18 +91,35 @@ void MeshDevice::stopSearch(void)
   mSearching = false;
 }
 
-void MeshDevice::registerNeighbor(MeshDevice* device)
+void MeshDevice::registerNeighbor(ble_adv_addr_t* advAddr, uint32_t rxTime)
 {
+  mNeighbors.push_back(MeshNeighbor(advAddr, rxTime));
+  if (mNeighbors.size() == )
 
+}
+
+MeshNeighbor* MeshDevice::getNeighbor(ble_adv_addr_t* advAddr)
+{
+  for (MeshNeighbor& nb : mNeighbors)
+  {
+    if (nb.mAdvAddr == *advAddr)
+    {
+      return &nb;
+    }
+  }
+
+  return NULL;
 }
 
 void MeshDevice::transmit(uint8_t* data, uint32_t length)
 {
-  mPacketQueue.push(new RadioPacket(mRadio, data, length));
-  
+  ble_adv_packet_t* pPacket = new ble_adv_packet_t();
+  memcpy(pPacket->adv_addr.arr, mMyAddr.arr, BLE_ADV_ADDR_LEN);
+
+  mPacketQueue.push(pPacket);
 }
 
-void MeshDevice::setDefaultPacket(uint8_t* data, uint32_t length)
+void MeshDevice::addRoute(ble_adv_addr_t* addr, uint32_t dist)
 {
 
 }
@@ -62,5 +138,45 @@ void MeshDevice::radioCallbackRx(RadioPacket* packet, uint8_t rx_strength, bool 
   ble_adv_packet_t blePacket;
   memcpy(&blePacket, packet->getContents(), packet->getLength());
 
-  blePacket.
+  MeshNeighbor* pNb = getNeighbor(&blePacket.adv_addr);
+  if (pNb == NULL)
+  {
+    registerNeighbor(&blePacket.adv_addr, packet->mStartTime);
+    if ()
+  }
+  else
+  {
+    pNb->receivedBeacon(packet->mStartTime);
+  }
+
+  
+}
+
+route_entry_t* MeshDevice::getWeakestRoute(void)
+{
+  route_entry_t* pOldest = &mRoutes[0];
+
+  for (uint32_t i = 1; i < MESH_MAX_ROUTES; ++i)
+  {
+    if (pOldest->timeStamp == 0) // quick exit before route table is filled
+      return pOldest;
+
+    if (mRoutes[i].timeStamp < pOldest->timeStamp)
+      pOldest = &mRoutes[i];
+  }
+
+  return pOldest;
+}
+
+MeshNeighbor* MeshDevice::getStrongestSubscription(void)
+{
+  if (mSubscriptions.size() == 0)
+    return NULL;
+
+  MeshNeighbor* pStrongest = mSubscriptions[0];
+
+  for (auto it = mSubscriptions.begin(); it != mSubscriptions.end(); it++)
+  {
+    (*it)->
+  }
 }
--- MeshTesting/MeshDevice.h
+++ MeshTesting/MeshDevice.h
@@ -1,9 +1,116 @@
 #pragma once
 #include "Device.h"
 #include "RadioPacket.h"
+#include "BlePacket.h"
 #include <vector>
 #include <queue>
 #include <stdint.h>
+
+#define MESH_INTERVAL           (100*MS)
+#define MESH_MAX_SUBSCRIPTIONS  (5)
+#define MESH_MAX_CLOCK_DRIFT    (250.0 * PPM)
+#define MESH_CH_OFFSET_US(offs) (625 * ((uint32_t) offs)
+#define MESH_MAX_ROUTES         (10)
+#define MESH_UNKNOWN_DIST       (UINT32_MAX)
+
+#define MESH_ACCESS_ADDR        (0xC0221E55)
+
+class MeshDevice;
+
+//mesh packet, based on ble adv
+#pragma pack(push, 1)
+typedef enum
+{
+  MESH_ADV_TYPE_DEFAULT = 0x50,
+  MESH_ADV_TYPE_NEIGHBOR_NOTIFICATION,
+  MESH_ADV_TYPE_ACK,
+  MESH_ADV_TYPE_UNICAST,
+  MESH_ADV_TYPE_BROADCAST
+}mesh_adv_type_t;
+
+typedef struct
+{
+  uint32_t access_addr;
+  ble_packet_header_t header;
+  ble_adv_addr_t adv_addr;
+  union
+  {
+    uint8_t raw[250];
+    struct
+    {
+      uint8_t adv_len;    // BLE compatibility
+      mesh_adv_type_t adv_type;  // BLE compatibility
+      union 
+      {
+        uint8_t raw[248];
+        struct
+        {
+          ble_adv_addr_t clusterAddr; // address of cluster head
+          uint16_t offsetFromCH;      // number of 625us slots offset
+          uint8_t nbCount;            // number of other nodes this one is subscribed to
+          uint8_t powerScore;         // based on power supply and stability
+        } default;
+        struct
+        {
+          ble_adv_addr_t neighborsCH; // address of neighbor's clusterhead
+          ble_adv_addr_t neighborAddr;// address of neighbor
+          uint32_t nextExpectedBeacon;// timeoffset from this msg to the next expected nb beacon
+          uint8_t neighborScore;      // neighbor's powerScore
+        } neighborNotification;
+        union
+        {
+          struct
+          {
+            ble_adv_addr_t sender;    // multihop source
+            ble_adv_addr_t dest;      // multihop destination
+            uint32_t sender_dist;     // distance from sender
+            uint32_t dest_dist;       // distance to destination
+            uint8_t payload[228];     // rest of packet is payload
+          } unicast;
+          struct
+          {
+            ble_adv_addr_t sender;
+            uint8_t payload[242];
+          } broadcast;
+        }data;
+      } payload;
+    } str;
+  }payload;
+} mesh_packet_t;
+#pragma pack(pop)
+
+typedef struct
+{
+  ble_adv_addr_t destination;
+  uint32_t timeStamp;
+  uint32_t distance;
+} route_entry_t;
+
+class MeshNeighbor
+{
+  friend MeshDevice;
+public:
+  MeshNeighbor(ble_adv_addr_t* addr);
+
+  void receivedBeacon(uint32_t rxTime, mesh_packet_t* beacon);
+
+  bool isClusterHead(void);
+
+  uint8_t getPowerScore(void) { return mPowerScore; }
+  uint32_t getLastSyncTime(void) { return mLastSyncTime; }
+  uint32_t getNextBeaconTime(uint32_t timeNow);
+    
+  ble_adv_addr_t mAdvAddr;
+  bool mFollowing;
+private:
+  ble_adv_addr_t mClusterHead;
+
+  uint32_t mLastSyncTime;
+  uint32_t mNbCount;
+  uint8_t mPowerScore;
+  uint32_t mBeaconCount;
+  uint32_t mBeaconInterval;
+};
 
 class MeshDevice : public Device
 {
@@ -15,20 +122,31 @@ public:
   void startSearch(void);
   void stopSearch(void);
 
-  void registerNeighbor(MeshDevice* device);
+  void registerNeighbor(ble_adv_addr_t* advAddr, uint32_t rxTime);
+  MeshNeighbor* getNeighbor(ble_adv_addr_t* advAddr);
 
   void transmit(uint8_t* data, uint32_t length);
-  void setDefaultPacket(uint8_t* data, uint32_t length);
+  
+  void setClusterHead(MeshNeighbor* nb);
+  void setPowerScore(uint8_t score);
+  void addRoute(ble_adv_addr_t* addr, uint32_t dist);
+  
+  uint32_t getDistanceTo(ble_adv_addr_t* addr);
 
 private:
-  std::vector<MeshDevice*> mNeighbors;
-  std::queue<RadioPacket*> mPacketQueue;
-  RadioPacket* mDefaultPacket = NULL;
+  route_entry_t mRoutes[MESH_MAX_ROUTES];
+  std::vector<MeshNeighbor> mNeighbors;
+  std::queue<ble_adv_packet_t*> mPacketQueue;
+  std::vector<MeshNeighbor*> mSubscriptions;
+  mesh_packet_t mDefaultPacket;
   bool mSearching;
-  ble_adv_addr_t myAddr;
+  ble_adv_addr_t mMyAddr;
 
   virtual void radioCallbackTx(RadioPacket* packet);
   virtual void radioCallbackRx(RadioPacket* packet, uint8_t rx_strength, bool corrupted);
+
+  route_entry_t* getWeakestRoute(void);
+  MeshNeighbor* getStrongestSubscription(void);
 };
 
   
