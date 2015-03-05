5716781777e932a5c38f8440539498ed88c5f331
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2015-03-05 18:53:09 +0100

    Moved to purely macrobased logging. Fuck C++ cout

--- wsn_sim/Logger.cpp
+++ wsn_sim/Logger.cpp
@@ -1,12 +1,4 @@
 #include "Logger.h"
 #include <mutex>
 
-std::mutex Logger::mCoutMutex;
-
-std::ostream& operator<<(std::ostream& os, const Logger& log)
-{
-  Logger::mCoutMutex.lock();
-  os << std::endl << log.mPrefix << ": " << log.mStream.str();
-  Logger::mCoutMutex.unlock();
-  return os;
-}
+SimEnv* pLoggerSimEnv = NULL;
--- wsn_sim/Logger.h
+++ wsn_sim/Logger.h
@@ -1,52 +1,55 @@
 #pragma once
+#include "SimEnv.h"
 #include <string>
 #include <iostream>
 #include <sstream>
 #include <mutex>
 #include <stdarg.h>
 
-#define LOG_ENABLE        (0)
+#if defined(_WIN32) || defined(_WIN64)
+#include <windows.h>
+#endif
 
-#define LOG_ENABLE_ERROR  (1)
-#define LOG_ENABLE_DEBUG  (1)
-#define LOG_ENABLE_VERBOSE (1)
+#ifndef LOG_ENABLE  
+  #define LOG_ENABLE        (1)
+#endif
 
-class Logger
-{
-public:
-  Logger(std::string prefix, bool enabled) : mPrefix(prefix), mEnabled(enabled) {};
+extern SimEnv* pLoggerSimEnv;
 
-  Logger& operator<<(const Logger& log)
-  {
-    mStream << log.mStream.str();
-    return *this;
-  }
-
-  template <class T>
-  Logger& operator<<(const T &data)
-  {
-    if (mEnabled && LOG_ENABLE)
-      mStream << data;
-    return *this;
-  }
-
-
-  friend std::ostream& operator<<(std::ostream& os, const Logger& log);
-private:
-  std::stringstream mStream;
-  std::string mPrefix;
-  bool mEnabled;
-  static std::mutex mCoutMutex;
-};
-
-#define LOG_ERROR   std::cout << Logger("[ERROR][" + std::string(__FUNCTION__) + ":L" + std::to_string(__LINE__) + "]", true)
-#define LOG_DEBUG   std::cout << Logger("[DEBUG][" + std::string(__FUNCTION__) + ":L" + std::to_string(__LINE__) + "]", true)
-#define LOG_VERBOSE std::cout << Logger("[VERBOSE][" + std::string(__FUNCTION__) + ":L" + std::to_string(__LINE__) + "]", true)
-
-#define LOGGER(prefix) std::cout << Logger(prefix, true)
+#if defined(_WIN32) || defined(_WIN64)
+#define LOG_COLOR(col) do{ HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); SetConsoleTextAttribute(hConsole, col); } while(0)
+#define LOG_COLOR_RESET LOG_COLOR(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY)
+#define __PRETTY_FILE__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__) 
+#else
+#define LOG_COLOR(col)
+#define LOG_COLOR_RESET
+#define __PRETTY_FILE__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__) 
+#endif
 
 #if LOG_ENABLE
-  #define _LOG(str, ...) printf("[%s]: " str "\n", __FUNCTION__, __VA_ARGS__)
+#define _LOG(str, ...) do {\
+if (!LOG_ENABLE) break; \
+  LOG_COLOR_RESET; \
+  printf("[%s:L%d][@%d]: " str "\n", __PRETTY_FILE__, __LINE__, \
+  ((pLoggerSimEnv != NULL) ? pLoggerSimEnv->getTimestamp() : 0), __VA_ARGS__); \
+} while (0)
+
+#define _ERROR(str, ...) do {\
+if (!LOG_ENABLE) break; \
+  LOG_COLOR(FOREGROUND_RED | FOREGROUND_INTENSITY); \
+  printf("[%s:L%d][@%d][ERROR]: " str "\n", __PRETTY_FILE__, __LINE__, \
+  ((pLoggerSimEnv != NULL) ? pLoggerSimEnv->getTimestamp() : 0), __VA_ARGS__); \
+  system("pause"); exit(0); \
+  } while (0)
+
+#define _WARN(str, ...) do {\
+  if (!LOG_ENABLE) break; \
+  LOG_COLOR(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);\
+  printf("[%s:L%d][@%d][WARN]: " str "\n", __PRETTY_FILE__, __LINE__,\
+  ((pLoggerSimEnv != NULL)? pLoggerSimEnv->getTimestamp() : 0), __VA_ARGS__);\
+  LOG_COLOR_RESET; } while(0)
 #else
-  #define _LOG(str, ...) 
+#define _LOG(str, ...) 
+#define _ERROR(str, ...)
+#define _WARN(str, ...)
 #endif
